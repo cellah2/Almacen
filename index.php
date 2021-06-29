251e59bacba52e94d74322ec7c943df3366e4cb2
cellah2 <45906934+cellah2@users.noreply.github.com>
2021-06-29 03:04:59 -0400

    Login y Creacion modulo ventas
    -Adaptacion de login sin usar modals.
    -Cambio en variables para verificar que sea un administrador en cada modulo de administracion.
    -Creacion de modulo de ventas

--- index.php
+++ index.php
@@ -13,47 +13,38 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="/assets/script.js"></script>
-    <title>Bienvenido | Control Inventario</title>
+    <title>Almacen de Keka | Administracion y Ventas</title>
 
 </head>
 <body>
-    <div class="jumbotron center">
-    <h1 class="display-4"> Administracion de Productos</h1>
-    
-    <p class="lead">Bienvenido al control de inventario, aca podrás modificar y agregar productos, agregar o quitar categorias, etc</p>
-    <hr class="my-4">    
-    <!-- <a class="btn btn-success btn-lg" href="login.php">Iniciar Sesion</a> -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login">
-  Iniciar Sesion
-</button>
-    </div>
 
-
-
-
-<!-- Modal -->
-<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Iniciar Sesion</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <form action="validar.php" method="post">
-            <label for="email">Correo o Usuario:</label>
-            <input type="text" name="email" placeholder="Ingresar usuario">
-            <label for="password">Contraseña:</label>
-            <input type="password" name="clave" placeholder="Ingresa tu contraseña">
-            <input type="submit" value="Ingresar">
-        </form>
-      </div>
-
+<div class="global-container">
+  <div class="card login-form">
+    <div class="card-body">
+      <h1 class="card-title text-center"> Administracion de Productos</h1>
+      <h3 class="card-text text-center"> Bienvenido/a al Almacen Keka </h6>
+        <div class="card-text">
+          <form action="validar.php" method="post">
+           <div class="form-group">
+             <label for="exampleInputEmail1">Email o usuario</label>
+             <input type="text" name="email" class="form-control form-control-sm" id="exampleInputEmail1" >
+           </div>
+           <div class="form-group">
+             <label for="exampleInputPassword1">Contraseña</label>
+             <a href="#" style="float:right;font-size:12px;">Olvidaste la contraseña?</a>
+             <input type="password" name="clave" class="form-control form-control-sm" id="exampleInputPassword1">
+           </div>
+             <button type="submit" class="btn btn-primary btn-block">Iniciar sesion</button>
+          </form>
+        </div>
     </div>
   </div>
 </div>
+
+
+    
+
+        
 
 </body>
 </html>

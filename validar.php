5eba3c5d68496ce00e286f961914eb731cdeadd9
unknown <m4tyxd@gmail.com>
2021-07-09 23:04:51 -0400

    Inicio con Nombre
    Validacion con nombre y email en inicio de sesion
    Cambio modal to container en login style
    Validacion en sesiones de administrador y usuario

--- validar.php
+++ validar.php
@@ -10,25 +10,26 @@ $conn=mysqli_connect("localhost","root","","almacenkeka");
 
 $consulta="SELECT * FROM usuarios WHERE email='$email' or nombre='$email' and password='$clave'";
 $resultado = mysqli_query($conn,$consulta);
-$fila['priv'] = '';
 
 $filas=mysqli_num_rows($resultado);
 if($filas>0){
     session_start();
     $fila = $resultado->fetch_assoc();
-    echo $fila['priv'];
-    if ($fila['priv'] == 'admin'){
+    $_SESSION['var'] = $fila['priv'];
+    $_SESSION['email'] = $email;
+    /*if ($fila['priv'] == 'admin'){
         $_SESSION['email'] = $email;
         $_SESSION['var'] = $fila['priv'];
         header("location:dashboard.php");
     }else{
         $_SESSION['email'] = $email;
-        $_SESSION['var'] = $fila['priv'];
+        //$_SESSION['var'] = $fila['priv'];
         //header("location:dashboard.php");
         header("location:ventas.php");
-    } 
+    } */
 
 
+    header("location:ventas.php");
 
 }else {
     echo "<script> alert('Este usuario no existe, intente denuevo'); 
--- ventas.php
+++ ventas.php
@@ -26,6 +26,7 @@ include("conexion.php");
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abel|Poiret+One|ZCOOL+XiaoWei&display=swap">
 </head>
 <body>
+    <!--
 <div class="col-md-12 ml-auto bg-secondary fixed-top top-navbar">
     <div class="row align-items-center">
         <div class="col-md-6">
@@ -33,7 +34,10 @@ include("conexion.php");
         </div>
         <div class="col-md-6">
         <ul class="navbar-nav">
-        <?php if ($_SESSION['var'] = 'admin'){?>
+        
+        <?php
+        if ($_SESSION['var'] == 'admin'){
+                    ?>
                     <li class="nav-item ml-md-auto"><a href="dashboard.php" class="nav-link text-dark bg-primary border border-primary rounded my-2">Panel de Administracion<i class="fas fa-sign-out-alt text-dark fa-lg"></i></a></li>
                     <?php
                 }?>
@@ -42,8 +46,20 @@ include("conexion.php");
         </div>
     </div>
 </div>
-
-
+  -->
+<div class="row align-items-center bg-secondary d-flex justify-content-between">
+<div class="col-3">
+    <h5 class="text-light mb-0">Almacen Keka</h4>
+</div>
+<div class="col-6">
+    <h2 style="margin-top: 20px;">MODULO DE VENTAS</h2>
+</div>
+<div class="col-3">
+<?php if ($_SESSION['var'] == 'admin'){ ?>
+        <a href="dashboard.php" class="btn btn-primary btn-sm">Panel de Administración<i class="fas fa-chart-bar text-dark fa-lg"></i></a>
+<?php } ?>
+        <a href="" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#sign-out">Cerrar Sesión<i class="fas fa-sign-out-alt text-dark fa-lg"></i></a>
+</div>
 
 
 <?php include("includes/footer.php"); 
